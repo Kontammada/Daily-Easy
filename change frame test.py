69762e9e08b391e054e42922dbe74c52905898e6
Sutthinai Boonyingyongchai <s.kontammada@gmail.com>
2014-11-23 12:10:38 +0700

    app can makr folder 'Note-Data' if first run thisa app and the folder is not created

--- change frame test.py
+++ change frame test.py
@@ -30,18 +30,21 @@ class StartPage(tk.Frame):
         label.image = photo #save reference of photo
         button1 = tk.Button(self, image = photo,command=lambda: controller.show_frame(MainPage))
         button1.pack()
-
+        work_dir = os.getcwd()
+        check = os.listdir(work_dir)
+        if 'Note-Data' not in check:
+            os.mkdir("Note-Data")
 
 class MainPage(tk.Frame):
     def __init__(self, parent, controller):
-        
+
         tk.Frame.__init__(self, parent) 
         label = tk.Label(self, text="This is page 1")
         label.pack(side="top", fill="x", pady=10)
         main_button = tk.Button(self, text="Back to Intro", command=lambda: controller.show_frame(StartPage))
         button2 = tk.Button(self, text="New", command=lambda: controller.show_frame(New))
         button3 = tk.Button(self, text="Go to the start page", command=lambda: controller.show_frame(StartPage))
-        
+
         main_button.pack()
         button2.pack()
         button3.pack()

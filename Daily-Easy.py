55ad89f067edf15109885fac06c4aacc16b39110
Sutthinai Boonyingyongchai <s.kontammada@gmail.com>
2014-12-06 22:02:39 +0700

    edit for able to show note follow tag button

--- Daily-Easy.py
+++ Daily-Easy.py
@@ -1,5 +1,7 @@
 import Tkinter as tk
 import os
+from os import listdir
+from os.path import isfile, join
 import datetime
 
 
@@ -42,37 +44,37 @@ class MainPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="main _/|\_")
         label.pack(side="top", fill="x", pady=10)
-        self.text1 = tk.Text(self)
-        self.text1.config(width=33, heigh=5)
-        self.text1.place(x=10, y=40)
-        self.text2 = tk.Text(self)
-        self.text2.config(width=33, heigh=5)
-        self.text2.place(x=10, y=130)
-        self.text3 = tk.Text(self)
-        self.text3.config(width=33, heigh=5)
-        self.text3.place(x=10, y=220)
-        self.text4 = tk.Text(self)
-        self.text4.config(width=33, heigh=5)
-        self.text4.place(x=10, y=310)
         self.edge = tk.Label(self, text="========================================")
         self.edge.place(y = 400)
-        self.workbutton = tk.Button(self, text="Work *-*)//", command=lambda: controller.show_frame(Work))
+        self.workbutton = tk.Button(self, text="Work *-*)//", command=lambda: controller.show_frame(Work),relief='groove')
         self.workbutton.place(x=5, y=425)
-        self.housebutton = tk.Button(self, text="House _(:3 JL)_", command=lambda: controller.show_frame(House))
+        self.housebutton = tk.Button(self, text="House _(:3 JL)_", command=lambda: controller.show_frame(House),relief='groove')
         self.housebutton.place(x=80, y=425)
-        self.introbutton = tk.Button(self, text="Back to Intro", command=lambda: controller.show_frame(StartPage))
+        self.introbutton = tk.Button(self, text="Back to Intro", command=lambda: controller.show_frame(StartPage),relief='groove')
         self.introbutton.place(x=5, y=465)
-        self.newbutton = tk.Button(self, text="New", command=lambda: controller.show_frame(New))
+        self.newbutton = tk.Button(self, text="New", command=lambda: controller.show_frame(New),relief='groove')
         self.newbutton.place(x=5, y=500)
-        self.aboutbutton = tk.Button(self, text="About", command=lambda: controller.show_frame(About))
+        self.aboutbutton = tk.Button(self, text="About", command=lambda: controller.show_frame(About),relief='groove')
         self.aboutbutton.place(x=5, y=535)
-        
-        canvas = tk.Canvas(width=80, height=80)
-        vbar=tk.Scrollbar(self,orient='vertical')
-        vbar.pack(side='right',fill='y')
-        vbar.config(command=canvas.yview)
-        canvas.config(yscrollcommand=vbar.set)
-        canvas.place(self, x=10, y=60)
+        frame = tk.Frame(self)
+        frame.place(x=7,y=40)
+        scrollbar = tk.Scrollbar(frame)
+        scrollbar.pack(side='right', fill='y')
+        listbox = tk.Listbox(frame,width=45, height=22)
+        listbox.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=listbox.yview)
+        listbox.bind("<Double-Button-1>", self.OnDouble)
+        listbox.pack(side="top", fill="both", expand=True)
+        now = os.getcwd()
+        onlyfiles = [ f for f in listdir(now+'''/Note-Data''') if isfile(join(now+'''/Note-Data''',f)) ]
+        onlyfiles.sort()
+        for j in onlyfiles:
+            listbox.insert('end',j[:-27])
+    def OnDouble(self, event):
+        widget = event.widget
+        selection=widget.curselection()
+        value = widget.get(selection[0])
+        print "selection:", selection, ": '%s'" % value
 
 
 
@@ -94,7 +96,14 @@ class New(tk.Frame):
         self.text.config(width=35, heigh=15)
         self.text.insert('1.0', 'my status here.\n')
         self.text.place(x=10, y=130)
-        self.button = tk.Button(self, text="Save", command=self.on_button, relief='flat')
+        #work or house radiobutton
+        self.tag_var = tk.StringVar()
+        work = tk.Radiobutton(self, text='Work', variable=self.tag_var, value='works')
+        house = tk.Radiobutton(self, text='House', variable=self.tag_var, value='house')
+        work.place(x=200,y=420)
+        house.place(x=200,y=440)
+        #save button
+        self.button = tk.Button(self, text="Save", command=self.save_note)
         self.button.place(x=260, y=395)
         self.list_m = ['1','2','3','4','5','6','7','8','9','10','11','12']
         self.dict = {'1':range(1,32), '2':range(1,30), '3':range(1,32),
@@ -116,11 +125,13 @@ class New(tk.Frame):
         self.optionmenu_b.place(x=70, y=390)
         self.optionmenu_c.place(x=130, y=390)
         self.pack()
-    def on_button(self):
+        
+    def save_note(self):
         date = str(datetime.datetime.now().date())
         title = self.text_title.get('1.0', 'end-1c')
         alert = str(self.variable_c.get())+'-'+str(self.variable_a.get()).zfill(2)+'-'+str(self.variable_b.get()).zfill(2)
-        name = date+' __ '+alert+" _ "+str(title)
+        tag = [str(self.tag_var.get()),'works'][str(self.tag_var.get())=='']
+        name = alert+" _ "+str(title)+' __ '+date+'   #'+tag
         data = self.text.get('1.0', 'end-1c')
         new_file = open("Note-Data/"+name+".txt", "w+")
         alert = str(self.variable_c.get())+'-'+str(self.variable_a.get())+'-'+str(self.variable_b.get())
@@ -134,7 +145,7 @@ class New(tk.Frame):
         self.optionmenu_b.pack_forget()
         self.optionmenu_b = tk.OptionMenu(self, self.variable_b, *value_a)
         self.variable_b.set(1)
-        self.optionmenu_b.place(x=70, y=390)    
+        self.optionmenu_b.place(x=70, y=390)
 
 
 
@@ -143,18 +154,6 @@ class Work(tk.Frame):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Workkkkkk!!!")
         label.pack(side="top", fill="x", pady=10)
-        self.text1 = tk.Text(self)
-        self.text1.config(width=33, heigh=5)
-        self.text1.place(x=10, y=40)
-        self.text2 = tk.Text(self)
-        self.text2.config(width=33, heigh=5)
-        self.text2.place(x=10, y=130)
-        self.text3 = tk.Text(self)
-        self.text3.config(width=33, heigh=5)
-        self.text3.place(x=10, y=220)
-        self.text4 = tk.Text(self)
-        self.text4.config(width=33, heigh=5)
-        self.text4.place(x=10, y=310)
         self.edge = tk.Label(self, text="========================================")
         self.edge.place(y = 400)
         self.workbutton = tk.Button(self, text="Work *-*)//", command=lambda: controller.show_frame(Work))
@@ -167,8 +166,26 @@ class Work(tk.Frame):
         self.newbutton.place(x=5, y=500)
         self.aboutbutton = tk.Button(self, text="About", command=lambda: controller.show_frame(About))
         self.aboutbutton.place(x=5, y=535)
-
-
+        frame = tk.Frame(self)
+        frame.place(x=7,y=40)
+        scrollbar = tk.Scrollbar(frame)
+        scrollbar.pack(side='right', fill='y')
+        listbox = tk.Listbox(frame,width=45, height=22)
+        listbox.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=listbox.yview)
+        listbox.bind("<Double-Button-1>", self.OnDouble)
+        listbox.pack(side="top", fill="both", expand=True)
+        now = os.getcwd()
+        onlyfiles = [ f for f in listdir(now+'''/Note-Data''') if isfile(join(now+'''/Note-Data''',f)) ]
+        onlyfiles.sort()
+        for j in onlyfiles:
+            if '#works' in j:
+                listbox.insert('end',j[:-27])
+    def OnDouble(self, event):
+        widget = event.widget
+        selection=widget.curselection()
+        value = widget.get(selection[0])
+        print "selection:", selection, ": '%s'" % value
 
 
 class House(tk.Frame):
@@ -176,18 +193,6 @@ class House(tk.Frame):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="House =3=")
         label.pack(side="top", fill="x", pady=10)
-        self.text1 = tk.Text(self)
-        self.text1.config(width=33, heigh=5)
-        self.text1.place(x=10, y=40)
-        self.text2 = tk.Text(self)
-        self.text2.config(width=33, heigh=5)
-        self.text2.place(x=10, y=130)
-        self.text3 = tk.Text(self)
-        self.text3.config(width=33, heigh=5)
-        self.text3.place(x=10, y=220)
-        self.text4 = tk.Text(self)
-        self.text4.config(width=33, heigh=5)
-        self.text4.place(x=10, y=310)
         self.edge = tk.Label(self, text="========================================")
         self.edge.place(y = 400)
         self.workbutton = tk.Button(self, text="Work *-*)//", command=lambda: controller.show_frame(Work))
@@ -200,8 +205,26 @@ class House(tk.Frame):
         self.newbutton.place(x=5, y=500)
         self.aboutbutton = tk.Button(self, text="About", command=lambda: controller.show_frame(About))
         self.aboutbutton.place(x=5, y=535)
-
-
+        frame = tk.Frame(self)
+        frame.place(x=7,y=40)
+        scrollbar = tk.Scrollbar(frame)
+        scrollbar.pack(side='right', fill='y')
+        listbox = tk.Listbox(frame,width=45, height=22)
+        listbox.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=listbox.yview)
+        listbox.bind("<Double-Button-1>", self.OnDouble)
+        listbox.pack(side="top", fill="both", expand=True)
+        now = os.getcwd()
+        onlyfiles = [ f for f in listdir(now+'''/Note-Data''') if isfile(join(now+'''/Note-Data''',f)) ]
+        onlyfiles.sort()
+        for j in onlyfiles:
+            if '#house' in j:
+                listbox.insert('end',j[:-27])
+    def OnDouble(self, event):
+        widget = event.widget
+        selection=widget.curselection()
+        value = widget.get(selection[0])
+        print "selection:", selection, ": '%s'" % value
 
 
 if __name__ == "__main__":

25661ffa4dda6927766376f243033ab543d48e03
Sutthinai Boonyingyongchai <s.kontammada@gmail.com>
2014-12-07 00:25:55 +0700

    edit many many many but it not work hahaha

--- Daily-Easy.py
+++ Daily-Easy.py
@@ -75,77 +75,7 @@ class MainPage(tk.Frame):
         selection=widget.curselection()
         value = widget.get(selection[0])
         print "selection:", selection, ": '%s'" % value
-
-
-
-class New(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent) 
-        label = tk.Label(self, text="New Ja!!!")
-        label.place(x=130, y=30)
-        main_button = tk.Button(self, text="Go to the main page", command=lambda: controller.show_frame(MainPage))
-        main_button.place(x=95, y=550)
-        self.title = tk.Label(self, text='Title')
-        self.title.place(x=10, y=60)
-        self.description = tk.Label(self, text='Description')
-        self.description.place(x=10, y=110)
-        self.text_title = tk.Text(self)
-        self.text_title.config(width=35, heigh=1)
-        self.text_title.place(x=10, y=80)
-        self.text = tk.Text(self)
-        self.text.config(width=35, heigh=15)
-        self.text.insert('1.0', 'my status here.\n')
-        self.text.place(x=10, y=130)
-        #work or house radiobutton
-        self.tag_var = tk.StringVar()
-        work = tk.Radiobutton(self, text='Work', variable=self.tag_var, value='works')
-        house = tk.Radiobutton(self, text='House', variable=self.tag_var, value='house')
-        work.place(x=200,y=420)
-        house.place(x=200,y=440)
-        #save button
-        self.button = tk.Button(self, text="Save", command=self.save_note)
-        self.button.place(x=260, y=395)
-        self.list_m = ['1','2','3','4','5','6','7','8','9','10','11','12']
-        self.dict = {'1':range(1,32), '2':range(1,30), '3':range(1,32),
-                     '4':range(1,31), '5':range(1,32), '6':range(1,31),
-                     '7':range(1,32), '8':range(1,32), '9':range(1,31),
-                     '10':range(1,32), '11':range(1,31), '12':range(1,32)}
-        self.year = range(2014, 2031)
-        self.variable_a = tk.StringVar(self)
-        self.variable_b = tk.StringVar(self)
-        self.variable_c = tk.StringVar(self)
-        self.variable_a.trace('w', self.update_b)
-        self.optionmenu_a = tk.OptionMenu(self, self.variable_a, *self.list_m)
-        self.optionmenu_b = tk.OptionMenu(self, self.variable_b, *self.dict['1'])
-        self.optionmenu_c = tk.OptionMenu(self, self.variable_c, *self.year)
-        self.variable_a.set('1')
-        self.variable_b.set(1)
-        self.variable_c.set(2014)
-        self.optionmenu_a.place(x=10, y=390)
-        self.optionmenu_b.place(x=70, y=390)
-        self.optionmenu_c.place(x=130, y=390)
-        self.pack()
-        
-    def save_note(self):
-        date = str(datetime.datetime.now().date())
-        title = self.text_title.get('1.0', 'end-1c')
-        alert = str(self.variable_c.get())+'-'+str(self.variable_a.get()).zfill(2)+'-'+str(self.variable_b.get()).zfill(2)
-        tag = [str(self.tag_var.get()),'works'][str(self.tag_var.get())=='']
-        name = alert+" _ "+str(title)+' __ '+date+'   #'+tag
-        data = self.text.get('1.0', 'end-1c')
-        new_file = open("Note-Data/"+name+".txt", "w+")
-        alert = str(self.variable_c.get())+'-'+str(self.variable_a.get())+'-'+str(self.variable_b.get())
-        new_file.write(data)
-        self.text.delete('1.0', 'end')
-        self.text_title.delete('1.0', 'end')
-        self.success = tk.Label(self, text='...File saved...')
-        self.success.place(x=117, y=440)
-    def update_b(self, *args):
-        value_a = self.dict[self.variable_a.get()]
-        self.optionmenu_b.pack_forget()
-        self.optionmenu_b = tk.OptionMenu(self, self.variable_b, *value_a)
-        self.variable_b.set(1)
-        self.optionmenu_b.place(x=70, y=390)
+        Edit()
 
 
 
@@ -225,6 +155,145 @@ class House(tk.Frame):
         selection=widget.curselection()
         value = widget.get(selection[0])
         print "selection:", selection, ": '%s'" % value
+
+class New(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent) 
+        label = tk.Label(self, text="New Ja!!!")
+        label.place(x=130, y=30)
+        main_button = tk.Button(self, text="Go to the main page", command=lambda: controller.show_frame(MainPage))
+        main_button.place(x=95, y=550)
+        self.title = tk.Label(self, text='Title')
+        self.title.place(x=10, y=60)
+        self.description = tk.Label(self, text='Description')
+        self.description.place(x=10, y=110)
+        self.text_title = tk.Text(self)
+        self.text_title.config(width=35, heigh=1)
+        self.text_title.place(x=10, y=80)
+        self.text = tk.Text(self)
+        self.text.config(width=35, heigh=15)
+        self.text.insert('1.0', 'my status here.\n')
+        self.text.place(x=10, y=130)
+        #work or house radiobutton
+        self.tag_var = tk.StringVar()
+        work = tk.Radiobutton(self, text='Work', variable=self.tag_var, value='works')
+        house = tk.Radiobutton(self, text='House', variable=self.tag_var, value='house')
+        work.place(x=200,y=420)
+        house.place(x=200,y=440)
+        #save button
+        self.button = tk.Button(self, text="Save", command=self.save_note)
+        self.button.place(x=260, y=395)
+        self.list_m = ['1','2','3','4','5','6','7','8','9','10','11','12']
+        self.dict = {'1':range(1,32), '2':range(1,30), '3':range(1,32),
+                     '4':range(1,31), '5':range(1,32), '6':range(1,31),
+                     '7':range(1,32), '8':range(1,32), '9':range(1,31),
+                     '10':range(1,32), '11':range(1,31), '12':range(1,32)}
+        self.year = range(2014, 2031)
+        self.variable_a = tk.StringVar(self)
+        self.variable_b = tk.StringVar(self)
+        self.variable_c = tk.StringVar(self)
+        self.variable_a.trace('w', self.update_b)
+        self.optionmenu_a = tk.OptionMenu(self, self.variable_a, *self.list_m)
+        self.optionmenu_b = tk.OptionMenu(self, self.variable_b, *self.dict['1'])
+        self.optionmenu_c = tk.OptionMenu(self, self.variable_c, *self.year)
+        self.variable_a.set('1')
+        self.variable_b.set(1)
+        self.variable_c.set(2014)
+        self.optionmenu_a.place(x=10, y=390)
+        self.optionmenu_b.place(x=70, y=390)
+        self.optionmenu_c.place(x=130, y=390)
+        self.pack()
+        
+    def save_note(self):
+        date = str(datetime.datetime.now().date())
+        title = self.text_title.get('1.0', 'end-1c')
+        alert = str(self.variable_c.get())+'-'+str(self.variable_a.get()).zfill(2)+'-'+str(self.variable_b.get()).zfill(2)
+        tag = [str(self.tag_var.get()),'works'][str(self.tag_var.get())=='']
+        name = alert+" _ "+str(title)+' __ '+date+'   #'+tag
+        data = self.text.get('1.0', 'end-1c')
+        new_file = open("Note-Data/"+name+".txt", "w+")
+        alert = str(self.variable_c.get())+'-'+str(self.variable_a.get())+'-'+str(self.variable_b.get())
+        new_file.write(data)
+        self.text.delete('1.0', 'end')
+        self.text_title.delete('1.0', 'end')
+        self.success = tk.Label(self, text='...File saved...')
+        self.success.place(x=117, y=440)
+    def update_b(self, *args):
+        value_a = self.dict[self.variable_a.get()]
+        self.optionmenu_b.pack_forget()
+        self.optionmenu_b = tk.OptionMenu(self, self.variable_b, *value_a)
+        self.variable_b.set(1)
+        self.optionmenu_b.place(x=70, y=390)
+
+
+class Edit(object):
+    def __init__(self):
+        edit_window = tk.Tk()
+        edit_window.geometry('300x600-10+50')
+        label = tk.Label(self, text="Edit")
+        label.place(x=130, y=30)
+        '''self.title = tk.Label(self, text='Title')
+        self.title.place(x=10, y=60)
+        self.description = tk.Label(self, text='Description')
+        self.description.place(x=10, y=110)
+        self.text_title = tk.Text(self)
+        self.text_title.config(width=35, heigh=1)
+        self.text_title.place(x=10, y=80)
+        self.text = tk.Text(self)
+        self.text.config(width=35, heigh=15)
+        self.text.insert('1.0', 'my status here.\n')
+        self.text.place(x=10, y=130)
+        #work or house radiobutton
+        self.tag_var = tk.StringVar()
+        work = tk.Radiobutton(self, text='Work', variable=self.tag_var, value='works')
+        house = tk.Radiobutton(self, text='House', variable=self.tag_var, value='house')
+        work.place(x=200,y=420)
+        house.place(x=200,y=440)
+        #save button
+        self.button = tk.Button(self, text="Save", command=self.save_note)
+        self.button.place(x=260, y=395)
+        self.list_m = ['1','2','3','4','5','6','7','8','9','10','11','12']
+        self.dict = {'1':range(1,32), '2':range(1,30), '3':range(1,32),
+                     '4':range(1,31), '5':range(1,32), '6':range(1,31),
+                     '7':range(1,32), '8':range(1,32), '9':range(1,31),
+                     '10':range(1,32), '11':range(1,31), '12':range(1,32)}
+        self.year = range(2014, 2031)
+        self.variable_a = tk.StringVar(self)
+        self.variable_b = tk.StringVar(self)
+        self.variable_c = tk.StringVar(self)
+        self.variable_a.trace('w', self.update_b)
+        self.optionmenu_a = tk.OptionMenu(self, self.variable_a, *self.list_m)
+        self.optionmenu_b = tk.OptionMenu(self, self.variable_b, *self.dict['1'])
+        self.optionmenu_c = tk.OptionMenu(self, self.variable_c, *self.year)
+        self.variable_a.set('1')
+        self.variable_b.set(1)
+        self.variable_c.set(2014)
+        self.optionmenu_a.place(x=10, y=390)
+        self.optionmenu_b.place(x=70, y=390)
+        self.optionmenu_c.place(x=130, y=390)
+        self.pack()
+        
+    def save_note(self):
+        date = str(datetime.datetime.now().date())
+        title = self.text_title.get('1.0', 'end-1c')
+        alert = str(self.variable_c.get())+'-'+str(self.variable_a.get()).zfill(2)+'-'+str(self.variable_b.get()).zfill(2)
+        tag = [str(self.tag_var.get()),'works'][str(self.tag_var.get())=='']
+        name = alert+" _ "+str(title)+' __ '+date+'   #'+tag
+        data = self.text.get('1.0', 'end-1c')
+        new_file = open("Note-Data/"+name+".txt", "w+")
+        alert = str(self.variable_c.get())+'-'+str(self.variable_a.get())+'-'+str(self.variable_b.get())
+        new_file.write(data)
+        self.text.delete('1.0', 'end')
+        self.text_title.delete('1.0', 'end')
+        self.success = tk.Label(self, text='...File saved...')
+        self.success.place(x=117, y=440)
+    def update_b(self, *args):
+        value_a = self.dict[self.variable_a.get()]
+        self.optionmenu_b.pack_forget()
+        self.optionmenu_b = tk.OptionMenu(self, self.variable_b, *value_a)
+        self.variable_b.set(1)
+        self.optionmenu_b.place(x=70, y=390)'''
+
 
 
 if __name__ == "__main__":
